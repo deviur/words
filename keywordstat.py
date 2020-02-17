c62bec089e3de1dd35f604a170ae43881f70b2c7
Deviur <deviur@yandex.ru>
2020-02-17 12:18:52 +0300

    RELEASE v0.1.1

--- keywordstat.py
+++ keywordstat.py
@@ -7,7 +7,10 @@ else
 fi
 exit $?
 
-Это консольное приложение для подсчёта частотности слов в запросах, по которым пришли насайт
+
+Программа подсчёта количества кликов по ключевым словам в поисковой системе Яндекс.
+
+Программа принимает csv-файл из выгрузки Яндекс.Вебмастера и выдаёт результать на экран.
 
 Использование:
 keywordstat.py < yandex-stat-file.csv
@@ -17,12 +20,13 @@ import csv
 import re
 import sys
 
-VERSION = '0.1'
+VERSION = '0.1.1'
 
+
+# Загрузить csv-файл для обработки
 input_csv = csv.DictReader(sys.stdin)
-all_words = {}
 
-# Подсчёт количества кликов (clicks) на сайт по каждому запросу (query).
+# Подсчёт общего количества кликов (clicks) по каждому запросу (query).
 queries = []
 for line in input_csv:
     for key, value in line.items():
@@ -34,28 +38,25 @@ for line in input_csv:
 
     queries.append({'Query': query, 'Clicks': clicks})
 
-print(queries)
+# print(queries)
 # Результат:
 # [{'Query': 'Несколько винипухов', 'Clicks': 18.0}, {'Query': 'Винипухи сильнее всех', 'Clicks': 13.0},
 # {'Query': 'без винипухов никуда', 'Clicks': 10.0}, {'Query': 'долой винипухов', 'Clicks': 7.0},
 # {'Query': 'сильнее винипуха', 'Clicks': 7.0}, {'Query': 'всех на винипухов', 'Clicks': 4.0}]
-
 # TODO Сделать сортировку. Как?
 
-# Получить список слов из запросов и сумму кликов
-all_words = {}
+# Подсчёт общего количества кликов (clicks) по каждому ключевому слову (keywords).
+keywords = {}
 for query in queries:
-    # PEP 8: invalid escape sequence '\w' For us it means that flake8 is upset
-    words = re.findall('\w+', query['Query'])
+    words = re.findall('[a-zA-Zа-яА-ЯёЁ]+', query['Query'])
 
     for word in words:
-        if word in all_words:
-            all_words[word]['Query'].append(query['Query'])
-            all_words[word]['Clicks'] += query['Clicks']
+        if word in keywords:
+            keywords[word] += query['Clicks']
         else:
-            all_words[word] = {'Clicks': query['Clicks'], 'Query': [query['Query']]}
+            keywords[word] = query['Clicks']
 
-print(all_words)
+# print(keywords)
 # Результат:
 # {'Несколько': {'Clicks': 18.0, 'Query': ['Несколько винипухов']},
 # 'винипухов': {'Clicks': 39.0, 'Query': ['Несколько винипухов', 'без винипухов никуда',
@@ -67,5 +68,11 @@ print(all_words)
 #                                                                        'Query': ['без винипухов никуда']},
 # 'долой': {'Clicks': 7.0, 'Query': ['долой винипухов']}, 'винипуха': {'Clicks': 7.0, 'Query': ['сильнее винипуха']},
 # 'на': {'Clicks': 4.0, 'Query': ['всех на винипухов']}}
-
 # TODO Переделать программу. Использовать def-функции. Упросить вывод результата.
+
+
+# Вывод результатов
+list_d = list((keywords.items()))
+list_d.sort(key=lambda i: i[1])
+list_d.reverse()
+print(list_d)
